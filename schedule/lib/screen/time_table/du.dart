a157d7d63f6df9b83b31552031d012bcc63c3df4
Tung <nguyenduytung@TungNguyens-Mac-Buc.local>
2024-03-05 11:30:37 +0700

    test1

--- schedule/lib/screen/time_table/du.dart
+++ schedule/lib/screen/time_table/du.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Hello extends StatefulWidget {
   const Hello({Key? key}) : super(key: key);
@@ -10,6 +11,6 @@ class Hello extends StatefulWidget {
 class _HelloState extends State<Hello> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp();
   }
 }
